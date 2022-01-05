08f30647beb90e12567af48744de69c5c7390c63
User <koboshove@gmail.com>
2022-01-05 14:17:24 +0600

    Эрмек

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        int a = 2;
+        for (int i = 1; i <=10; i++) {
+            System.out.println(a+"*"+i+"="+2*i);
+
+        }
+
+    }
+}
